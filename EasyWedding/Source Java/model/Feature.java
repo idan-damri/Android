71fb9a0e25df23ea30eed81c697bba7c9bec7e49
idan-damri <idandamri12@gmail.com>
2020-08-03 01:09:26 +0300

    update model

--- EasyWedding/Source Java/model/Feature.java
+++ EasyWedding/Source Java/model/Feature.java
@@ -1,45 +1,97 @@
 package com.example.easywedding.model;
 
-public class Feature {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import com.google.firebase.database.Exclude;
+
+public class Feature implements Parcelable {
 
     private String name;
+    private String supplierId;
+    private String advancePayment;
+    private String paymentBalance;
     private String freeText;
-    private String supplierName;
-    private String supplierPhoneNumber;
-    private int advancePayment;
-    private int paymentBalance;
+    private String quantity;
 
-    public Feature(String name, String freeText, String supplierName
-                   , String supplierPhoneNumber, int advancePayment, int paymentBalance) {
+    public Feature(){
+
+    }
+    public Feature(String name, String supplierId, String freeText
+                   ,String advancePayment, String paymentBalance, String quantity) {
         this.name = name;
+        this.supplierId = supplierId;
         this.freeText = freeText;
-        this.supplierName = supplierName;
-        this.supplierPhoneNumber = supplierPhoneNumber;
         this.advancePayment = advancePayment;
         this.paymentBalance = paymentBalance;
+        this.quantity = quantity;
     }
+    protected Feature(Parcel in) {
+        name = in.readString();
+        supplierId = in.readString();
+        advancePayment = in.readString();
+        paymentBalance = in.readString();
+        freeText = in.readString();
+        quantity = in.readString();
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(name);
+        dest.writeString(supplierId);
+        dest.writeString(advancePayment);
+        dest.writeString(paymentBalance);
+        dest.writeString(freeText);
+        dest.writeString(quantity);
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    public static final Creator<Feature> CREATOR = new Creator<Feature>() {
+        @Override
+        public Feature createFromParcel(Parcel in) {
+            return new Feature(in);
+        }
+
+        @Override
+        public Feature[] newArray(int size) {
+            return new Feature[size];
+        }
+    };
 
     public String getName() {
         return name;
+    }
+
+    public String getQuantity() {
+        return quantity;
+    }
+
+    public String getSupplierId() {
+        return supplierId;
+    }
+
+    public void setQuantity(String quantity) {
+        this.quantity = quantity;
+    }
+
+    public void setSupplierId(String supplierId) {
+        this.supplierId = supplierId;
     }
 
     public String getFreeText() {
         return freeText;
     }
 
-    public String getSupplierName() {
-        return supplierName;
-    }
 
-    public String getSupplierPhoneNumber() {
-        return supplierPhoneNumber;
-    }
-
-    public int getAdvancePayment() {
+    public String getAdvancePayment() {
         return advancePayment;
     }
 
-    public int getPaymentBalance() {
+    public String getPaymentBalance() {
         return paymentBalance;
     }
 
@@ -51,19 +103,12 @@ public class Feature {
         this.freeText = freeText;
     }
 
-    public void setSupplierName(String supplierName) {
-        this.supplierName = supplierName;
-    }
 
-    public void setSupplierPhoneNumber(String supplierPhoneNumber) {
-        this.supplierPhoneNumber = supplierPhoneNumber;
-    }
-
-    public void setAdvancePayment(int advancePayment) {
+    public void setAdvancePayment(String advancePayment) {
         this.advancePayment = advancePayment;
     }
 
-    public void setPaymentBalance(int paymentBalance) {
+    public void setPaymentBalance(String paymentBalance) {
         this.paymentBalance = paymentBalance;
     }
     
